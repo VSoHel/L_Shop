06ae4a22faaef0fee909858073e0cf2db298a9d8
VSoHel <vssonich@gmail.com>
2026-03-24 09:05:09 +0300

    feat: подключение routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 
+import authRoutes from './routes/auth.routes';
+import cartRoutes from './routes/cart.routes';
+
 const app = express();
 
 app.use(cors({
@@ -11,5 +14,8 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use('/auth', authRoutes);
+app.use('/cart', cartRoutes);
 
 export default app;
