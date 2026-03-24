7d06eadd9db311798b9428c13a45c862f38435ef
VSoHel <vssonich@gmail.com>
2026-03-24 08:34:13 +0300

    fix:  изменение сообщения

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -4,7 +4,7 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     const userId = req.cookies.userId;
 
     if (!userId) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: 'Пользователь не авторизованн' });
     }
     next();
 };
